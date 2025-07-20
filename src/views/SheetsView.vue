16c6cd3be0fa9b59bee6aed409c89fadcbbfe44d
James Tuppenney <james.tuppenney@hotmail.co.uk>
2025-07-20 12:31:19 +0100

    Follow Pinia tutorial

--- src/views/SheetsView.vue
+++ src/views/SheetsView.vue
@@ -1,3 +1,15 @@
+<script setup>
+    import { useCounterStore } from ".././stores/counter";
+
+    const store = useCounterStore();
+    store.count++;
+    store.increment(2);
+
+</script>
+
 <template>
-    <p>HELLOOOO THIS IS THE SHEETS PAGE OK?</p>
+    <h1>Count is {{store.count}}</h1>
+    <h2>DoubleCount is {{store.doubleCount}}</h2>
+    <button @click="store.increment(1)">Increment</button>
+    <button @click="store.waitAndAdd">Wait + Increment</button>
 </template>
