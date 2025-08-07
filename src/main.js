18a6299202383b22274cf2309ce36a55d6d2728f
James Tuppenney <james.tuppenney@hotmail.co.uk>
2025-08-07 12:55:08 +0100

    Dynamic header title and improve header icon

--- src/main.js
+++ src/main.js
@@ -1,6 +1,6 @@
 import './assets/main.css'
 
-import { createApp } from 'vue'
+import { createApp,nextTick } from 'vue'
 import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
@@ -9,3 +9,12 @@ createApp(App)
     .use(router)
     .use(createPinia())
     .mount('#app')
+
+//https://stackoverflow.com/questions/51639850/how-to-change-page-titles-when-using-vue-router
+router.afterEach((to, from) => {
+    // Use next tick to handle router history correctly
+    // see: https://github.com/vuejs/vue-router/issues/914#issuecomment-384477609
+    nextTick(() => {
+        document.title = "BlindBase | " + to.name;
+    });
+});
