fb16728f23959db62896055db701ff96ce45725b
James Tuppenney <james.tuppenney@hotmail.co.uk>
2025-10-15 07:44:27 +0100

    Begin recon preview from recon selection

--- src/components/recons/ReconSelect.vue
+++ src/components/recons/ReconSelect.vue
@@ -15,33 +15,57 @@
         })
     }
 
+    const selectedRecon = ref(-1)
     function ReconClicked(index) {
-        router.push(`/reconCreate`).then(() => {
+        selectedRecon.value = index
+        /*router.push(`/reconCreate`).then(() => {
             router.push(`/recons/${reconsStore.recons[index].scramble}`)
-        })
+        })*/
     }
 </script>
 
 <template>
     <div class="ReconSelect">
-        <div style="font-size:3rem;"><u><b>Reconstructions:</b></u></div>
-        <div style="width:40%;font-size:2rem;">
-            <List :data="reconsStore.recons.map((recon) => recon.name)"
-                  :selectedIndex="-1"
-                  @onItemClick="ReconClicked" />
+        <div id="COLUMN1">
+            <div v-if="selectedRecon != -1">
+                <pre id="reconBodyPreview">{{reconsStore.recons[selectedRecon].body}}</pre>
+            </div>
         </div>
-        <div>
-            <input v-model="newScramble" />
-            <button @click="createRecon">Create</button>
+        <div id="COLUMN2">
+            <div style="font-size:3rem;"><u><b>Reconstructions:</b></u></div>
+            <div style="width:100%;font-size:1.5rem;">
+                <List :data="reconsStore.recons.map((recon) => recon.name)"
+                      :selectedIndex="selectedRecon"
+                      @onItemClick="ReconClicked" />
+            </div>
+            <div>
+                <input v-model="newScramble" />
+                <button @click="createRecon">Create</button>
+            </div>
+            <div id="COLUMN3">
+
+            </div>
         </div>
     </div>
 </template>
 
 <style>
     .ReconSelect{
+        display:grid;
+        grid-template-columns: 35% 30% 35%;
+        color: var(--grey-100);
+        height: 93vh;
+    }
+
+    #reconBodyPreview {
+        font-size: 1.5rem;
+        width: 100%;
+        text-wrap: pretty;
+    }
+
+    #COLUMN2{
         display:flex;
         flex-direction: column;
-        color: var(--grey-100);
         align-items: center;
     }
 
