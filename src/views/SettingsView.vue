fc6ec392da53144a1b0eac88618b59e0b90746d6
James Tuppenney <james.tuppenney@hotmail.co.uk>
2026-02-13 20:08:37 +0000

    Exporting and importing save data

--- src/views/SettingsView.vue
+++ src/views/SettingsView.vue
@@ -1,12 +1,65 @@
 <script setup>
-    import { computed } from 'vue'
+    import { computed, inject } from 'vue'
     import { useCardStore } from '@/stores/CardStore.js'
     import { defaults, useSettingsStore } from '.././stores/SettingsStore'
     const settingsStore = useSettingsStore()
     settingsStore.loadState()
+    
+    const confirmDialog = inject('confirmDialog')
 
     function SettingUpdated() {
         settingsStore.saveState()
+    }
+
+    async function ImportAll() {
+        if (!(await confirmDialog.value.open('Do you want to import BlindBase data? *This will delete and overwrite anything currently saved on BlindBase*.'))) {
+            return;
+        }
+        //https://stackoverflow.com/questions/16215771/how-to-open-select-file-dialog-via-js
+        var input = document.createElement('input')
+        input.type = 'file'
+        input.onchange = async e => {
+            var file = e.target.files[0]
+            const content = await new Promise((resolve, reject) => {
+                const reader = new FileReader()
+                reader.readAsText(file, 'UTF-8')
+                reader.onload = e => resolve(e.target.result)
+                reader.onerror = reject
+            })
+            
+            const jsonData = JSON.parse(content);
+            for(var key of Object.keys(jsonData)) {
+                console.log(key);
+                console.log(JSON.stringify(jsonData[key]));
+                localStorage.setItem(key, JSON.stringify(jsonData[key]));
+            }
+            window.location.reload();
+        }
+        input.click()
+    }
+
+    async function ExportAll() {
+        if (!(await confirmDialog.value.open('Export all data stored in BlindBase?'))) {
+            return;
+        }
+        var keys = Object.keys(localStorage).filter(key => key.includes('Store'));
+        var jsonString = '{';
+        for (var i = 0; i < keys.length; i++ ) {
+            jsonString += '\"' + keys[i] + '\": ';
+            jsonString += localStorage.getItem(keys[i]);
+            if(i < keys.length - 1)
+                jsonString += ',';
+        }
+        jsonString += '}';
+    
+        const encodedUri = "data:text/csv;charset=utf-8," + encodeURIComponent(jsonString);
+    
+        const link = document.createElement("a")
+        link.setAttribute("href", encodedUri)
+        link.setAttribute("download", "BlindBase_Data.json")
+        document.body.appendChild(link)
+        link.click();
+        document.body.removeChild(link)
     }
 </script>
 
@@ -138,7 +191,11 @@
                     </select>
                 </div>
             </div>
-
+            <div style="display:flex;flex-direction:row;gap:20px;">
+                <button @click="ImportAll"><h2>Import Data</h2></button>
+                <button @click="ExportAll"><h2>Export Data</h2></button>
+            </div>
+            
             <div style="min-height:200px" />
         </div>
     </div>
